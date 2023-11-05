e7aec7aade0174db096147d152b733a620a39ad8
nusrat-jahan7 <nusrat.jahan03060@gmail.com>
2023-11-05 11:03:00 +0600

    project setup

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Route";
+import { Toaster } from "react-hot-toast";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Toaster position="top-center" reverseOrder={false} />
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
